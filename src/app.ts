a818f555e907fffba50b57027ad5aa9c63654e18
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-05-13 17:43:52 -0300

    refactor: Adicionando o middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import errorMiddleware from './middlewares/errorMiddleware';
+import productsRouter from './routers/products';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/products', productsRouter);
+
+app.use(errorMiddleware);
 export default app;
