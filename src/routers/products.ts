1a10a77fed66f6a32943f0ccdd53821c42261659
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-05-13 22:35:57 -0300

    feat: Implementada a rota post para o product

--- src/routers/products.ts
+++ src/routers/products.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import ProductsController from '../controllers/products';
+import productValidate from '../middlewares/productValidate';
 
 const route: Router = Router();
 
 const productsController = new ProductsController();
 
 route
-  .get('/', productsController.getAll);
+  .get('/', productsController.getAll)
+  .post('/', productValidate, productsController.create);
 
 export default route;
