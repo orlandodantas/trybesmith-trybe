be1cb9b4df94934bbd2271bd72e7a64533b122b9
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-05-13 22:32:53 -0300

    feat: Implementado o create product para o model

--- src/models/products.ts
+++ src/models/products.ts
@@ -1,4 +1,4 @@
-import { Pool } from 'mysql2/promise';
+import { Pool, ResultSetHeader } from 'mysql2/promise';
 import Product from '../types/product.types';
 
 class ProductsModel {
@@ -12,6 +12,17 @@ class ProductsModel {
     const [products] = await this.connection.execute('SELECT * FROM Trybesmith.Products');
 
     return products as Product[];
+  }
+
+  public async create(product: Product): Promise<Product> {
+    const { name, amount } = product;
+
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+      'INSERT INTO Trybesmith.Products (name, amount) values (?, ?)',
+      [name, amount],
+    );
+
+    return { id: insertId, ...product } as Product;
   }
 }
 
