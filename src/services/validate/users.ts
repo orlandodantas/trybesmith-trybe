6f258ed2fa0aa7f43c630a357fdfe897f52ef132
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-05-15 10:45:30 -0300

    feat: Criando uma validacao para os dados obtidos do endpoint users

---
+++ src/services/validate/users.ts
@@ -0,0 +1,17 @@
+import Joi from 'joi';
+import User from '../../types/user.types';
+
+const schema = Joi.object({
+  username: Joi.string().min(3).required(),
+  classe: Joi.string().min(3).required(),
+  level: Joi.number().min(1).required(),
+  password: Joi.string().min(8).required(),
+});
+
+const validateFields = (user: User) => {
+  const { error } = schema.validate(user);
+
+  if (error) throw error;
+};
+
+export default validateFields;
