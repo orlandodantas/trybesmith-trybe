cc26e476eda527d03ad9ffb8bf45f782ad2b33ed
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-05-16 18:14:36 -0300

    feat: Implementado o create no controller para a rota orders

--- src/controllers/orders.ts
+++ src/controllers/orders.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import OrdersService from '../services/orders';
+import CustomRequest from '../types/express.types';
 import { OrderWithProducts } from '../types/order.types';
 
 class OrdersController {
@@ -14,6 +15,14 @@ class OrdersController {
     const orders: OrderWithProducts[] = await this.ordersService.getAll();
 
     res.status(StatusCodes.OK).json(orders);
+  };
+
+  public create = async (req: CustomRequest, res: Response): Promise<Response | void> => {
+    const { productsIds } = req.body;
+    const userId = req.userId as number;
+
+    const result = await this.ordersService.create({ userId, productsIds });
+    res.status(StatusCodes.CREATED).json(result);
   };
 }
 
