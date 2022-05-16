4cbae91ce0fb1d721f9e0c32c9ca2e1ef33076d8
Orlando Dantas <orlandodantas.1@hotmail.com>
2022-05-16 18:17:05 -0300

    feat: Implementando a rota implantando o create para o endpoint orders

--- src/routers/orders.ts
+++ src/routers/orders.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import OrdersController from '../controllers/orders';
+import auth from '../middlewares/auth';
+import orderValidate from '../middlewares/orderValidate';
 
 const route = Router();
 const ordersController = new OrdersController();
 
 route
-  .get('/', ordersController.getAll);
+  .get('/', ordersController.getAll)
+  .post('/', auth, orderValidate, ordersController.create);
 
 export default route;
